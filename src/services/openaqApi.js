fd8bb0bcb960cd49ddc1cf320561b63bbc70547e
Roy <charlesroylr@gmail.com>
2026-08-03 20:56:08 -0500

    fix: Usar proxy CORS para producción en GitHub Pages

--- src/services/openaqApi.js
+++ src/services/openaqApi.js
@@ -1,10 +1,7 @@
-// Usamos la URL real completa porque ya no tenemos el proxy de Vite
-const BASE_URL = 'https://api.openaq.org/v3';
+const BASE_URL = 'https://corsproxy.io/?https://api.openaq.org/v3';
 
-// Debes poner tu API key real aquí para que funcione en GitHub Pages
-const API_KEY = 'ce2a2e81863cd097439a61ab576c6054d06f0abfdf59a7a13cae0bd35cd760ef'; 
+const API_KEY = 'ce2a2e81863cd097439a61ab576c6054d06f0abfdf59a7a13cae0bd35cd760ef'; // Tu API Key real
 
-// Configuramos las cabeceras obligatorias de OpenAQ
 const fetchOptions = {
   headers: {
     'X-AQ-Key': API_KEY,

18ccaa3c14cc1bc635bc221fee5328de52879f7b
Roy <charlesroylr@gmail.com>
2026-08-03 20:36:39 -0500

    fix: Ajuste de basename en el enrutador para GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/web/">
       <div className="app-layout">
         <Sidebar />
         <main className="main-content">
